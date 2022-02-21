b1bf45a2cecea140781b31e8dff86e4be0f22512
Novianawati <novianawati92@gmailcom>
2022-02-21 23:30:16 +0700

    praktikum 2 langkah 2

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+use App\Http\Controllers\PageController;
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
